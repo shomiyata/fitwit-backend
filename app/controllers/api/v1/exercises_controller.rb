ac407dd7eb98ed1f3e46b88328373aaa44a96e47
shomiyata <eighteighttwenty@gmail.com>
2017-09-12 19:43:45 -0400

    transition from fetched data to data from db

--- app/controllers/api/v1/exercises_controller.rb
+++ app/controllers/api/v1/exercises_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::ExercisesController < ApplicationController
 
   def create
-    # byebug
     @exercise = Exercise.create(name: params[:name], description:params[:description], username:params[:username])
   end
 
   def index
+    byebug
     @exercises = Exercise.find_by(username: params[:username])
     render json: {exercises: @exercises}
   end
