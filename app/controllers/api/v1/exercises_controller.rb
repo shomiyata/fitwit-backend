b46513b5c6d554e5861761bfee6789ac0527cb2d
Jesse Tyner-Bryan <jesse@Jesses-MacBook-Air.local>
2017-09-12 20:57:18 -0400

    able to send users exercisesto front-end

--- app/controllers/api/v1/exercises_controller.rb
+++ app/controllers/api/v1/exercises_controller.rb
@@ -1,13 +1,15 @@
 class Api::V1::ExercisesController < ApplicationController
 
   def create
-    @exercise = Exercise.create(name: params[:name], description:params[:description], username:params[:username])
+    exercise = Exercise.create(name: params[:name], description:params[:description], username:params[:username])
   end
 
   def index
-    byebug
-    @exercises = Exercise.find_by(username: params[:username])
-    render json: {exercises: @exercises}
+    username = JSON.parse(request.headers["username"])["username"]
+    exercises = Exercise.where({username: username})
+    render json: {exercises: exercises}
   end
 
 end
+
+
